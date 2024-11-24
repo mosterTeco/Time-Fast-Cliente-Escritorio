e06770e2d72037ef9bf6b4e347aae072b85eaddc
mosterTeco <reyesmls11@hotmail.com>
2024-11-23 23:02:48 -0600

    Modulo de clientes incompleto,solo diseño

--- src/clientetimefastjavafx/FXMLMenuEnviosController.java
+++ src/clientetimefastjavafx/FXMLMenuEnviosController.java
@@ -5,9 +5,17 @@
  */
 package clientetimefastjavafx;
 
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
 
 /**
  * FXML Controller class
@@ -22,6 +30,21 @@ public class FXMLMenuEnviosController implements Initializable {
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
-    }    
-    
+    }
+
+    @FXML
+    private void OnClickCrearEnvio(ActionEvent event) {
+        try {
+            Stage escenarioSelec = new Stage();
+            Parent root = FXMLLoader.load(getClass().getResource("FXMLClientesDisponibles.fxml"));
+            Scene scene = new Scene(root);
+            escenarioSelec.setScene(scene);
+            escenarioSelec.setTitle("Seleccionar cliente");
+            escenarioSelec.initModality(Modality.APPLICATION_MODAL);
+            escenarioSelec.showAndWait();
+        } catch (IOException e) {
+
+        }
+    }
+
 }
