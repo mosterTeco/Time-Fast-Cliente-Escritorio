3fbc06cdb99921354d867550cddd645be9ff8ecf
mosterTeco <reyesmls11@hotmail.com>
2024-12-15 19:53:50 -0600

    CRUDEnvios

--- src/clientetimefastjavafx/FXMLFormularioEnviosController.java
+++ src/clientetimefastjavafx/FXMLFormularioEnviosController.java
@@ -5,9 +5,21 @@
  */
 package clientetimefastjavafx;
 
+import clientetimefastjavafx.modelo.dao.ClienteDAO;
+import clientetimefastjavafx.modelo.dao.EstadoDAO;
+import clientetimefastjavafx.modelo.dao.RolDAO;
+import clientetimefastjavafx.observador.NotificadorOperacion;
+import clientetimefastjavafx.pojo.Cliente;
+import clientetimefastjavafx.pojo.Colaborador;
+import clientetimefastjavafx.pojo.Envio;
+import clientetimefastjavafx.pojo.Estado;
+import clientetimefastjavafx.pojo.Rol;
 import java.io.IOException;
 import java.net.URL;
+import java.util.List;
 import java.util.ResourceBundle;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
@@ -16,6 +28,7 @@ import javafx.scene.Parent;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.ComboBox;
+import javafx.scene.control.TextField;
 import javafx.stage.Modality;
 import javafx.stage.Stage;
 
@@ -26,18 +39,74 @@ import javafx.stage.Stage;
  */
 public class FXMLFormularioEnviosController implements Initializable {
 
-    @FXML
-    private ComboBox<?> comboBoxRol;
-    @FXML
-    private Button buttonSeleccionarUnidad;
+    
+    private NotificadorOperacion observador;
 
+    private Envio envioEdicion;
+    @FXML
+    private TextField tfCalleO;
+    @FXML
+    private TextField tfNumeroO;
+    @FXML
+    private TextField tfCpO;
+    @FXML
+    private TextField tfCiudadO;
+    @FXML
+    private ComboBox<Estado> comboBoxEstado;
+    @FXML
+    private TextField tfDestino;
+    @FXML
+    private TextField tfNumeroGuia;
+    @FXML
+    private TextField tfCostoEnvio;
+    @FXML
+    private ComboBox<Estado> comboBoxEstatus;
+    @FXML
+    private TextField tfColoniaO;
+    @FXML
+    private ComboBox<Cliente> comboBoxClientes;
+    
+    List<Estado> estados = EstadoDAO.obtenerEstados();
+    
+    ObservableList<Estado> tiposEstados = FXCollections.observableArrayList(estados);
+    
+    List<Cliente> clientes = ClienteDAO.obtenerClientes();
+    
+    ObservableList<Cliente> tiposClientes = FXCollections.observableArrayList(clientes);
+    
     /**
      * Initializes the controller class.
      */
     @Override
     public void initialize(URL url, ResourceBundle rb) {
+        cargarEstados();
+        cargarClientes();
         // TODO
-    }    
+       
+        tfDestino.setEditable(false);
+        
+        comboBoxClientes.valueProperty().addListener((observable, oldValue, newValue) -> {
+        if (newValue != null) {
+            String direccion = newValue.getCalle() + " " +
+                               newValue.getColonia() + " " +
+                               newValue.getCp() + " " +
+                               newValue.getNumero();
+            tfDestino.setText(direccion);
+        }
+    });
+        
+    }
+    
+    public void inicializarValores(NotificadorOperacion observador, Envio envioEdicion) {
+        this.observador = observador;
+        this.envioEdicion = envioEdicion;
+
+        if (envioEdicion != null) {
+       
+        } else {
+
+        }
+    }
 
     @FXML
     private void OnClickSeleccionarConductor(ActionEvent event) {
@@ -52,6 +121,35 @@ public class FXMLFormularioEnviosController implements Initializable {
         } catch (IOException e) {
 
         }
+    }
+    
+    private void cargarEstados() {
+        if (estados != null && !estados.isEmpty()) {
+            System.out.println("Roles cargados: " + estados.size());
+            comboBoxEstado.setItems(tiposEstados);
+        } else {
+            System.out.println("No se pudieron cargar los estados");
+        }
+    }
+    
+    private void cargarClientes() {
+        if (clientes != null && !clientes.isEmpty()) {
+            System.out.println("Clientes cargados: " + clientes.size());
+            comboBoxClientes.setItems(tiposClientes);
+        } else {
+            System.out.println("No se pudieron cargar los estados");
+        }
+    }
+
+    
+    
+     private int buscarIdEstado(int idEstado) {
+        for (int i = 0; i < tiposEstados.size(); i++) {
+            if (tiposEstados.get(i).getId() == idEstado) {
+                return i;
+            }
+        }
+        return 0;
     }
     
 }
---
+++ src/clientetimefastjavafx/pojo/Envio.java
@@ -0,0 +1,272 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clientetimefastjavafx.pojo;
+
+/**
+ *
+ * @author reyes
+ */
+public class Envio {
+    
+    private Integer id;
+    private String numeroGuia;
+    private String calleOrigen;
+    private String coloniaOrigen;
+    private Integer numeroOrigen;
+    private String cpOrigen;
+    private String ciudadOrigen;
+    private String idEstadoOrigen;
+    private String estadoOrigen;
+    private String calleDestino;
+    private String coloniaDestino;
+    private Integer numeroDestino;
+    private String cpDestino;
+    private String ciudadDestino;
+    private String idEstadoDestino;
+    private String estadoDestino;
+    private Float costo;
+    private Integer idColaborador;
+    private String colaborador;
+    private Integer idCliente;
+    private String cliente;
+    private String correo;
+    private String telefono;
+    private Integer idEstatus;
+    private String estatus;
+    
+    public Envio() {
+    
+    }
+
+    public Envio(Integer id, String numeroGuia, String calleOrigen, String coloniaOrigen, Integer numeroOrigen, String cpOrigen, String ciudadOrigen, String idEstadoOrigen, String estadoOrigen, String calleDestino, String coloniaDestino, Integer numeroDestino, String cpDestino, String ciudadDestino, String idEstadoDestino, String estadoDestino, Float costo, Integer idColaborador, String colaborador, Integer idCliente, String cliente, String correo, String telefono, Integer idEstatus, String estatus) {
+        this.id = id;
+        this.numeroGuia = numeroGuia;
+        this.calleOrigen = calleOrigen;
+        this.coloniaOrigen = coloniaOrigen;
+        this.numeroOrigen = numeroOrigen;
+        this.cpOrigen = cpOrigen;
+        this.ciudadOrigen = ciudadOrigen;
+        this.idEstadoOrigen = idEstadoOrigen;
+        this.estadoOrigen = estadoOrigen;
+        this.calleDestino = calleDestino;
+        this.coloniaDestino = coloniaDestino;
+        this.numeroDestino = numeroDestino;
+        this.cpDestino = cpDestino;
+        this.ciudadDestino = ciudadDestino;
+        this.idEstadoDestino = idEstadoDestino;
+        this.estadoDestino = estadoDestino;
+        this.costo = costo;
+        this.idColaborador = idColaborador;
+        this.colaborador = colaborador;
+        this.idCliente = idCliente;
+        this.cliente = cliente;
+        this.correo = correo;
+        this.telefono = telefono;
+        this.idEstatus = idEstatus;
+        this.estatus = estatus;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNumeroGuia() {
+        return numeroGuia;
+    }
+
+    public void setNumeroGuia(String numeroGuia) {
+        this.numeroGuia = numeroGuia;
+    }
+
+    public String getCalleOrigen() {
+        return calleOrigen;
+    }
+
+    public void setCalleOrigen(String calleOrigen) {
+        this.calleOrigen = calleOrigen;
+    }
+
+    public String getColoniaOrigen() {
+        return coloniaOrigen;
+    }
+
+    public void setColoniaOrigen(String coloniaOrigen) {
+        this.coloniaOrigen = coloniaOrigen;
+    }
+
+    public Integer getNumeroOrigen() {
+        return numeroOrigen;
+    }
+
+    public void setNumeroOrigen(Integer numeroOrigen) {
+        this.numeroOrigen = numeroOrigen;
+    }
+
+    public String getCpOrigen() {
+        return cpOrigen;
+    }
+
+    public void setCpOrigen(String cpOrigen) {
+        this.cpOrigen = cpOrigen;
+    }
+
+    public String getCiudadOrigen() {
+        return ciudadOrigen;
+    }
+
+    public void setCiudadOrigen(String ciudadOrigen) {
+        this.ciudadOrigen = ciudadOrigen;
+    }
+
+    public String getIdEstadoOrigen() {
+        return idEstadoOrigen;
+    }
+
+    public void setIdEstadoOrigen(String idEstadoOrigen) {
+        this.idEstadoOrigen = idEstadoOrigen;
+    }
+
+    public String getEstadoOrigen() {
+        return estadoOrigen;
+    }
+
+    public void setEstadoOrigen(String estadoOrigen) {
+        this.estadoOrigen = estadoOrigen;
+    }
+
+    public String getCalleDestino() {
+        return calleDestino;
+    }
+
+    public void setCalleDestino(String calleDestino) {
+        this.calleDestino = calleDestino;
+    }
+
+    public String getColoniaDestino() {
+        return coloniaDestino;
+    }
+
+    public void setColoniaDestino(String coloniaDestino) {
+        this.coloniaDestino = coloniaDestino;
+    }
+
+    public Integer getNumeroDestino() {
+        return numeroDestino;
+    }
+
+    public void setNumeroDestino(Integer numeroDestino) {
+        this.numeroDestino = numeroDestino;
+    }
+
+    public String getCpDestino() {
+        return cpDestino;
+    }
+
+    public void setCpDestino(String cpDestino) {
+        this.cpDestino = cpDestino;
+    }
+
+    public String getCiudadDestino() {
+        return ciudadDestino;
+    }
+
+    public void setCiudadDestino(String ciudadDestino) {
+        this.ciudadDestino = ciudadDestino;
+    }
+
+    public String getIdEstadoDestino() {
+        return idEstadoDestino;
+    }
+
+    public void setIdEstadoDestino(String idEstadoDestino) {
+        this.idEstadoDestino = idEstadoDestino;
+    }
+
+    public String getEstadoDestino() {
+        return estadoDestino;
+    }
+
+    public void setEstadoDestino(String estadoDestino) {
+        this.estadoDestino = estadoDestino;
+    }
+
+    public Float getCosto() {
+        return costo;
+    }
+
+    public void setCosto(Float costo) {
+        this.costo = costo;
+    }
+
+    public Integer getIdColaborador() {
+        return idColaborador;
+    }
+
+    public void setIdColaborador(Integer idColaborador) {
+        this.idColaborador = idColaborador;
+    }
+
+    public String getColaborador() {
+        return colaborador;
+    }
+
+    public void setColaborador(String colaborador) {
+        this.colaborador = colaborador;
+    }
+
+    public Integer getIdCliente() {
+        return idCliente;
+    }
+
+    public void setIdCliente(Integer idCliente) {
+        this.idCliente = idCliente;
+    }
+
+    public String getCliente() {
+        return cliente;
+    }
+
+    public void setCliente(String cliente) {
+        this.cliente = cliente;
+    }
+
+    public String getCorreo() {
+        return correo;
+    }
+
+    public void setCorreo(String correo) {
+        this.correo = correo;
+    }
+
+    public String getTelefono() {
+        return telefono;
+    }
+
+    public void setTelefono(String telefono) {
+        this.telefono = telefono;
+    }
+
+    public Integer getIdEstatus() {
+        return idEstatus;
+    }
+
+    public void setIdEstatus(Integer idEstatus) {
+        this.idEstatus = idEstatus;
+    }
+
+    public String getEstatus() {
+        return estatus;
+    }
+
+    public void setEstatus(String estatus) {
+        this.estatus = estatus;
+    }
+
+}
